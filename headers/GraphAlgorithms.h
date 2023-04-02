f0d91925effb973674cc9a9d23c55a4ace2b7d9e
tomas <tomasarmento@gmail.com>
2023-04-02 17:56:00 +0100

    ok

--- headers/GraphAlgorithms.h
+++ headers/GraphAlgorithms.h
@@ -16,7 +16,21 @@ struct MaxTrainPair {
 
 class GraphAlgorithms: public Graph {
 public:
+    /**
+ * @brief Computes the maximum flow from source to sink using the Edmonds-Karp algorithm.
+ * @param source The ID of the source vertex.
+ * @param target The ID of the sink vertex.
+ * @return The maximum flow from source to sink.
+ *
+ * This function has a time complexity of O(V * E^2), where V is the number of vertices and E is the number of edges in the graph.
+ */
     int edmondsKarp(string source, string target);
+    /**
+ * @brief Finds the maximum flow between all pairs of vertices in the graph.
+ * @return A vector of MaxTrainPair objects representing the maximum flow between each pair of vertices.
+ *
+ * This function has a time complexity of O(V^3 * E^2), where V is the number of vertices and E is the number of edges in the graph.
+ */
     vector<MaxTrainPair> find_max_flow();
 protected:
     bool findArgumentingPath(Vertex* s,Vertex* t);
