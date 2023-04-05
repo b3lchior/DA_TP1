181dcf819f89d7dd5741c1398ba23327378e81a2
up202108777 <joaob3lchior@gmail.com>
2023-04-05 15:04:20 +0100

    Added doxygen to project

--- headers/Manager.h
+++ headers/Manager.h
@@ -10,10 +10,28 @@ class Manager {
 private:
     GraphAlgorithms graph_algorithms;
 public:
+    /**
+     * @brief Function that calls the functions that read the files
+     */
     void read_files();
+
+    /**
+     * @brief Function that reads the file stations.csv and adds the stations as vertexes to the graph
+     */
     void ReadStations();
+
+    /**
+     * @brief Function that reads the file network.csv and adds the routes as edges to the graph
+     */
     void ReadRoutes();
+
+    /**
+     * @brief Function that gets the network graph
+     * @return network graph
+     */
     GraphAlgorithms getGraph();
+
+
     void MaxFlow(string source, string target);
     void MaxFlowFromNetwork();
     int find_max_number_of_trains_to_station(string stationID);
